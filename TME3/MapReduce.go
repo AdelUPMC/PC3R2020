4ed3fb217619231402d2fcbc506355e90fe16cad
El Amraoui Adel <Elamraoui.Adel@hotmail.com>
2020-02-23 23:40:03 +0100

    TestCommit Sendpaquet

--- TME3/MapReduce.go
+++ TME3/MapReduce.go
@@ -40,62 +40,68 @@ type  message struct{
 }
 
 
-func lecteur(filename string, cl chan string,finlecteur chan int){
-		flag.Parse()
-		//filename =
- 	    //fmt.Println(filename)
-	    fmt.Println("Hello World")
+func lecteur(filename string, cl chan string,finlecteur chan int,termineTravailleurs chan int){
+  fmt.Println("Lancement du lecteur")
+  flag.Parse()
+  file, err := os.Open(filename)
+  if err != nil {
+          panic(err)
+  }
 
-         file, err := os.Open(filename)
-         if err != nil {
-                 panic(err)
-         }
+  defer file.Close()
 
-         defer file.Close()
+  reader := bufio.NewReader(file)
 
-         reader := bufio.NewReader(file)
+  reader.ReadLine() //skip the first line
+  for {
+          line, _, err := reader.ReadLine()
 
-         reader.ReadLine() //skip the first line
-         for {
-                 line, _, err := reader.ReadLine()
+          if err == io.EOF {
+            fmt.Println("fin du fichier ",line)
+            break;
+          }
+          /*envoit la ligne à un travailleur*/
+          cl<- string(line)
 
-                 if err == io.EOF {
-                   fmt.Println("fin du fichier ",line)
-                   break;
-                 }
-                 cl<- string(line)
+  }
+  termineTravailleurs<-0
+  finlecteur<-0
 
-         }
-         finlecteur<-0
+
 }
 
-func travailleur(cl chan string, cs chan message ,fin chan int){
-	line :=<-cl
-	fmt.Println("reçu ",line)
-	/*
-		convert line to paquet
-
-	*/
-	p := paquet{depart:"19:20:00",arrivee:"19:30:00",arret:500}
-	csout:=make(chan paquet)
-	m := message{content :p, cout : csout}
-	cs<-m
-	pnew:=<-csout
-	fmt.Println("Travailleur: arret =",pnew.arret)
-	fin<-0
+func travailleur(cl chan string, cs chan message ,fintravailleur chan int,termineTravailleurs chan int){
+  fmt.Println("Lancement d'un travailleur")
+  for{
+    select{
+    case line :=<-cl:
+    	   fmt.Println("reçu ",line)
+    	    p := paquet{depart:"19:20:00",arrivee:"19:30:00",arret:500}
+    	    csout:=make(chan paquet)
+    	    m := message{content :p, cout : csout}
+    	    cs<-m
+    	    pnew:=<-csout
+    	    fmt.Println("Travailleur: arret =",pnew.arret)
+    case <-termineTravailleurs:
+          fmt.Println("fin du travailleur")
+          fintravailleur<-0
+          break;
+      }
+    }
 }
 
 
-func  serveur (cs chan message,fin chan int ) {
-	  fmt.Println("Hello World2")
-		mess:=<-cs
+func  serveur (cs chan message,finserveur chan int ) {
+	  fmt.Println("Lancement du serveur")
+
+    mess:=<-cs
 		go func(m message){
 			m.content.arret=600
 			fmt.Println("Serveur: content =",m.content.arret)
 			m.cout <- m.content
 		}(mess)
 
-	fin<-0
+	finserveur<-0
 
 }
 
@@ -123,6 +129,7 @@ func main() {
  finlecteur := make ( chan int)
  fintravailleur := make ( chan int)
  finserveur := make ( chan int)
+termineTravailleur := make ( chan int)
  //finreducteur := make ( chan int)
 
 /*
@@ -130,8 +137,8 @@ func main() {
  creduct := make ( chan int)
 */
 
- go func(){lecteur("stop_times.txt",cl,finlecteur)}()
- go func(){travailleur(cl,cs,fintravailleur)}()
+ go func(){lecteur("stop_times.txt",cl,finlecteur,termineTravailleur)}()
+ go func(){travailleur(cl,cs,fintravailleur,termineTravailleur)}()
  go func(){serveur(cs,finserveur)}()
 
 
